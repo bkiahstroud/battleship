1fbb566b653cba2dcc99a37d4b0f58243446b74a
learn <learn@learn02-5.local>
2017-10-18 16:50:22 -0700

    fourth commit: added check board against array

--- battle.js
+++ battle.js
@@ -12,18 +12,26 @@ $(document).ready(function(){
   for (var i_tr = 0; i_tr < 10; i_tr++) {
     $("#battleTable").append("<tr id=tr_" +i_tr+">"); //create 10 trs with unique ids
     for (var i_td = 0; i_td < 10; i_td++) {
-      $("#battleTable").append("<td id=td_" +i_td+">"); //create 10 tds within each tr with unique ids
+      $("#battleTable").append("<td class=" + i_tr + " id=" + i_td + ">"); //create 10 tds within each tr with unique ids
    };
   };
-
   var torpedosRemaining = 25;
+  var win = 0;
   $("td").on("click", function(){ //need to add "if" statements to change color for either a hit or a miss.
+    if (board[parseInt($(this).attr("class"))][parseInt(event.target.id)] == 1) { // Return the value of the index in board at position [row][column]
+      $(this).addClass("hit");
+      win++;
+    } else {
+      $(this).addClass("miss");
+    }
     torpedosRemaining--;
-    $(this).addClass("miss");
     $("section").text("Torpedos remaining: " + torpedosRemaining);
     $(this).off("click");
-    console.log(torpedosRemaining);
-    if(torpedosRemaining <= 0) {
+    if (win == 5) {
+      alert("You have sunk all the battleships!");
+      $("td").off("click");
+    };
+    if (torpedosRemaining <= 0) {
       $("td").off("click");
       alert("Sorry you lose!");
     };
